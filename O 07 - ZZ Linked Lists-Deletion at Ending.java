feb0f13f2d160b77382e2b332a4f5e6962be349b
Sivabala-SBA <87258592+Sivabala-sba@users.noreply.github.com>
2023-08-17 09:27:47 +0530

    Create O 07 - ZZ Linked Lists-Deletion at Ending.java

---
+++ O 07 - ZZ Linked Lists-Deletion at Ending.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+}
+
+public class Solution {
+
+    static Node insertAtEnd(Node head, int val) {
+        Node newNode = new Node();
+        newNode.data = val;
+        newNode.next = null;
+
+        if (head == null) {
+            return newNode;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+
+        return head;
+    }
+
+    static Node deleteAtEnd(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        if (head.next == null) {
+            return null;
+        }
+
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+
+        return head;
+    }
+
+    static void printLinkedList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data);
+            if (current.next != null) {
+                System.out.print("->");
+            }
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt(); // Number of elements
+
+        Node head = null;
+
+        for (int i = 0; i < n; i++) {
+            int val = scanner.nextInt();
+            head = insertAtEnd(head, val);
+        }
+
+        int k = scanner.nextInt(); // k value for deletion
+
+        for (int i = 0; i < k; i++) {
+            head = deleteAtEnd(head);
+        }
+
+        printLinkedList(head);
+
+        scanner.close();
+    }
+}
